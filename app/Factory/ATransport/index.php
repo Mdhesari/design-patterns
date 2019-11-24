961247c1d31acbcf0b02ffffac1d428da19fe6a2
Mohamad Fazel <mdhesari99@gmail.com>
2019-11-24 12:37:02 +0330

    seperate abstract factory index.php

--- app/Factory/ATransport/index.php
+++ app/Factory/ATransport/index.php
@@ -1,2 +1,18 @@
 <?php
 
+
+use App\factory\Atransport\ATransportFactory;
+
+$transportFactory = new ATransportFactory;
+
+$trunk = $transportFactory->newRoadTransport();
+
+var_dump($trunk);
+
+$ship = $transportFactory->newSeaTransport();
+
+var_dump($ship);
+
+$jet = $transportFactory->newSkyTransfer();
+
+var_dump($jet);
--- index.php
+++ index.php
@@ -5,18 +5,3 @@
  */
 require_once __DIR__ . '/app/config/bootstrap.php';
 
-use App\factory\Atransport\ATransportFactory;
-
-$transportFactory = new ATransportFactory;
-
-$trunk = $transportFactory->newRoadTransport();
-
-var_dump($trunk);
-
-$ship = $transportFactory->newSeaTransport();
-
-var_dump($ship);
-
-$jet = $transportFactory->newSkyTransfer();
-
-var_dump($jet);
